adc237a425cd4bed17a421bc5b18068cc071472b
garlic0x1 <garlic1@protonmail.com>
2023-10-27 18:31:33 -0500

    readme and remove superfluous tables

--- src/db.lisp
+++ src/db.lisp
@@ -28,6 +28,7 @@
    (user :col-type (:varchar 128))
    (repo :col-type (:varchar 128))))
 
+;; one commit per repo
 (deftable last-commit ()
   ((repo :references (repository link) :primary-key t)
    (link :col-type (:varchar 256))
@@ -35,25 +36,29 @@
    (message :col-type (or :null :text))
    (time :col-type (or :null (:varchar 64)))))
 
-(deftable commit ()
-  ((link :col-type (:varchar 256) :primary-key t)
-   (author :col-type (:varchar 128))
-   (message :col-type (or :null :text))
-   (time :col-type (or :null (:varchar 64)))
-   (repo :references (repository link))))
+;;
+;; Not using these yet
+;;
 
-(deftable pull-request ()
-  ((link :col-type (:varchar 256) :primary-key t)
-   (author :col-type (:varchar 128))
-   (title :col-type (or :null (:varchar 256)))
-   (message :col-type (or :null :text))
-   (time :col-type (or :null (:varchar 64)))
-   (repo :references (repository link))))
+;; (deftable commit ()
+;;   ((link :col-type (:varchar 256) :primary-key t)
+;;    (author :col-type (:varchar 128))
+;;    (message :col-type (or :null :text))
+;;    (time :col-type (or :null (:varchar 64)))
+;;    (repo :references (repository link))))
 
-(deftable issue ()
-  ((link :col-type (:varchar 256) :primary-key t)
-   (author :col-type (:varchar 128))
-   (title :col-type (or :null (:varchar 256)))
-   (message :col-type (or :null :text))
-   (time :col-type (or :null (:varchar 64)))
-   (repo :references (repository link))))
+;; (deftable pull-request ()
+;;   ((link :col-type (:varchar 256) :primary-key t)
+;;    (author :col-type (:varchar 128))
+;;    (title :col-type (or :null (:varchar 256)))
+;;    (message :col-type (or :null :text))
+;;    (time :col-type (or :null (:varchar 64)))
+;;    (repo :references (repository link))))
+
+;; (deftable issue ()
+;;   ((link :col-type (:varchar 256) :primary-key t)
+;;    (author :col-type (:varchar 128))
+;;    (title :col-type (or :null (:varchar 256)))
+;;    (message :col-type (or :null :text))
+;;    (time :col-type (or :null (:varchar 64)))
+;;    (repo :references (repository link))))
--- src/mailer.lisp
+++ src/mailer.lisp
@@ -21,22 +21,23 @@
 
   (:method ((obj string)) (discord-send obj))
 
-  (:method ((obj db:commit))
-    (discord-send
-     (format nil "~a" (db::commit-link obj))
-     (db::commit-author obj)))
-
   (:method ((obj db:last-commit))
     (discord-send
      (format nil "commit: ~a~%time: ~a~%link: ~a" (db::last-commit-message obj) (db::last-commit-time obj) (db::last-commit-link obj))
      (db::last-commit-author obj)))
 
-  (:method ((obj db:issue))
-    (discord-send
-     (format nil "~a" (db::issue-link obj))
-     (db::issue-author obj)))
+  ;; (:method ((obj db:commit))
+  ;;   (discord-send
+  ;;    (format nil "~a" (db::commit-link obj))
+  ;;    (db::commit-author obj)))
 
-  (:method ((obj db:pull-request))
-    (discord-send
-     (format nil "~a" (db::pull-request-link obj))
-     (db::pull-request-author obj))))
+  ;; (:method ((obj db:issue))
+  ;;   (discord-send
+  ;;    (format nil "~a" (db::issue-link obj))
+  ;;    (db::issue-author obj)))
+
+  ;; (:method ((obj db:pull-request))
+  ;;   (discord-send
+  ;;    (format nil "~a" (db::pull-request-link obj))
+  ;;    (db::pull-request-author obj)))
+  )
