6c901528b359e87d9075c01f40c1a8ca33b119c4
garlic0x1 <garlic1@protonmail.com>
2023-11-02 03:44:09 -0500

    bbbb

--- src/core.lisp
+++ src/core.lisp
@@ -1,14 +1,22 @@
 (defpackage :gitwatch
   (:use :cl)
+  (:import-from #:cl-workers #:close-and-join-workers)
+  (:import-from #:mailer #:start-mailer #:*mailer*)
   (:export #:main))
 (in-package :gitwatch)
 
 (defun main (argv)
-  (mailer:start-mailer)
-  (mailer:alert-on-fail "gitwatch:main"
-    (config/secrets:db-connect)
-    (clingon:run gitwatch/cli:cli argv))
+  (start-mailer)
+  ;; (mailer:alert-on-fail "gitwatch:main")
+  (config/secrets:db-connect)
+  (clingon:run gitwatch/cli:cli argv)
 
   ;; wait for end
-  (cl-workers:close-worker mailer::*mailer*)
-  (cl-workers:join-worker mailer::*mailer*))
+  (close-and-join-workers mailer::*mailer*))
+
+(defun test-mailer ()
+  (start-mailer)
+  (loop :for i :from 0 :to 5
+        :do (mailer:send (write-to-string i)))
+  (close-and-join-workers mailer::*mailer*)
+  )
